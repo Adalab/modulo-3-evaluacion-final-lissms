c11d86b14973677405f138d0b1d9da165842525b
lissms <felissms@gmail.com>
2020-11-24 10:44:42 +0100

    add component datails

--- src/components/CharacterDetail.jsx
+++ src/components/CharacterDetail.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import api from "../service/api";
+import { Link } from "react-router-dom";
 
 function CharacterDetail(props) {
-  return <div>hola</div>;
+  let myObjetParam = useParams();
+
+  const [caracterDatails, setCharacterDetail] = useState({});
+
+  useEffect(() => {
+    api.getCharacterFromApi(myObjetParam.id).then((data) => {
+      setCharacterDetail(data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <img src={caracterDatails.image} alt="image" />
+      <h1>{caracterDatails.name}</h1>
+      <h5>{caracterDatails.status}</h5>
+      <h5>{caracterDatails.species}</h5>
+      <h5>{caracterDatails.origin}</h5>
+      <h5>{caracterDatails.episode}</h5>
+      <Link to="/">Volver</Link>
+    </div>
+  );
 }
 
 export default CharacterDetail;
--- src/service/api.js
+++ src/service/api.js
@@ -5,6 +5,22 @@ const getDataFromApi = () => {
       return data.results;
     });
 };
+const getCharacterFromApi = (id) => {
+  return fetch(`https://rickandmortyapi.com/api/character/${id}`)
+    .then((response) => response.json())
+    .then((data) => {
+      return {
+        image: data.image,
+        name: data.name,
+        status: data.status,
+        species: data.species,
+        origin: data.origin.name,
+        episode: data.episode.length,
+      };
+    });
+};
+
 export default {
   getDataFromApi,
+  getCharacterFromApi,
 };
